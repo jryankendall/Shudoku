1076c98426558a48955abbb57865a5abc451e3eb
JKRyan <jed@jedkendall.com>
2019-11-05 15:00:57 -0600

    Initial function creations, basic array population and dupe checking inmplemented.

--- public/assets/js/bin/printer.js
+++ public/assets/js/bin/printer.js
@@ -1,6 +1,23 @@
-$("#get-puzzle-btn").on("click", function(event) {
+/* $("#get-puzzle-btn").on("click", function(event) {
     console.log(event);
     todaysPuzzle( results => {
         console.log(results);
     })
-})
+}) */
+
+var linebreaksArr = [10, 19, 28, 37, 46, 55, 64, 73, 82];
+
+$("#get-puzzle-btn").on("click", function(event) {
+    console.log(event);
+    $("#example-puzzle-list").html(examplePuzzleString + "<br>" + examplePuzzleArr + "<br>" + examplePuzzleSolution);
+    for (let i = 1; i <= puzzleHeight*puzzleWidth; i++) {
+        if (linebreaksArr.indexOf(i) >= 0) {
+            $(".puzzle-container-area").append("<br>");
+        }
+        if (examplePuzzleArr[i] === ".") {
+            $(".puzzle-container-area").append("_ ")
+        } else {
+            $(".puzzle-container-area").append(examplePuzzleArr[i] + " ");
+        }
+    }
+});
--- public/assets/js/bin/solver.js
+++ public/assets/js/bin/solver.js
@@ -10,6 +10,58 @@ Show Possibles
 //URL: http://www.sudokuwiki.org/feed/fetchSudokuDAILYtxt.asp?accid=9002
 
 
+function NewArray() {
+    for (let i = 1; i <= 9; i++) {
+        this[i] = ["X"];
+        for (let j = 1; j <= 9; j++) {
+            this[i].push("");
+        };
+    };
+};
+
+function dupeCheck(arr) {
+    "use strict";
+    var result = {};
+
+    if (arr instanceof Array) {
+        arr.forEach((v, i) => {
+            if (!result[v]) {
+                result[v] = [i];
+            } else {
+                result[v].push(i);
+            }
+        })
+    }
+    return result;
+}
+
+function findDupes(item, index, arr) {
+    return arr.indexOf(item) != index;
+}
+
+var puzzleColumns = new NewArray();
+var puzzleRows = new NewArray();
+var puzzleBlocks = new NewArray();
+const uniqueNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(puzzleColumns);
+
+function buildArrays(pzlString) {
+    var inputString = pzlString.replace(/[.]/gi, "0");
+    for (let row = 1; row <= 9; row++) {
+        for (let column = 1; column <= 9; column++) {
+            puzzleColumns[column][row] = inputString.charAt((row-1)*9+column);            
+            puzzleRows[column][row] = inputString.charAt((column-1)*9+row);
+        }
+    }
+    console.log(puzzleColumns);
+    console.log(puzzleRows);
+    
+}
+
+buildArrays("s..1......6....9..35.2.6.84..7.8.....9.32.56.4.....3.7..69.3.5.22..9....8......4..");
+checkRow(1);
+
 function seedCells() {
     let puzzleSize = puzzleWidth * puzzleHeight;
     for (let i = 0; i < puzzleSize; i++) {
@@ -23,11 +75,28 @@ function getCellValue(cellX, cellY, cb) {
     return cb(cellArray[cellNum]);
 }
 
+function checkForSolved() {
+
+}
+
+function findPossibles() {
+
+}
+
+function checkRow(xVal, cb) {
+    var selectedRow = puzzleRows[xVal];
+    if (selectedRow.indexOf(0) >= 0) {
+        cb(false);
+    }
+
+    console.log(selectedRow.filter(findDupes));
+}
+
 //Test stuff, remove later V
-seedCells();
+/* seedCells();
 getCellValue(6, 9, function(val) {
     console.log(val);
 })
-console.log(examplePuzzleArr);
+console.log(examplePuzzleArr); */
 
 //
